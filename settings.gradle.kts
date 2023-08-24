336f006b4f546e764162131cc1f25149ab3964d1
Mikhail Mitrofanov <mk.mitrofamov@outlook.com>
2023-08-25 00:34:26 +0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+	repositories {
+		google()
+		mavenCentral()
+		gradlePluginPortal()
+	}
+}
+dependencyResolutionManagement {
+	@Suppress("UnstableApiUsage")
+	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+	@Suppress("UnstableApiUsage")
+	repositories {
+		google()
+		mavenCentral()
+	}
+}
+
+rootProject.name = "links"
+include(":app")
+ 
