a03b84ae8ffa5ce12d61ffe426097f4c489f918a
Mikhail Mitrofanov <mk.mitrofamov@outlook.com>
2023-08-27 01:12:53 +0300

    add dummy action for favorite menu

--- app/src/main/java/com/lampotrias/links/ui/list/LinksListFragment.kt
+++ app/src/main/java/com/lampotrias/links/ui/list/LinksListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
@@ -49,9 +50,8 @@ class LinksListFragment : Fragment() {
 		}
 
 		override fun onFavorite(linkModel: LinkModel) {
-
+			Toast.makeText(requireContext(), "Not supporting now", Toast.LENGTH_SHORT).show()
 		}
-
 	})
 
 	private fun navigateToEdit(linkModel: LinkModel) {
