24ead7ebe5f70ac7508b0d7dfd190b158d2ed7c0
Mikhail Mitrofanov <mk.mitrofamov@outlook.com>
2023-08-26 15:21:05 +0300

    add links list fragment and viewModel

--- app/src/main/java/com/lampotrias/links/LinksApplication.kt
+++ app/src/main/java/com/lampotrias/links/LinksApplication.kt
@@ -1,6 +1,7 @@
 package com.lampotrias.links
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-//@HiltAndroidApp
+@HiltAndroidApp
 class LinksApplication: Application()
---
+++ app/src/main/java/com/lampotrias/links/ui/list/LinksListViewModel.kt
@@ -0,0 +1,7 @@
+package com.lampotrias.links.ui.list
+
+import androidx.lifecycle.ViewModel
+
+class LinksListViewModel : ViewModel() {
+
+}
