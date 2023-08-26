a6186bf744d72a246419495bc6e9821d85989f01
Mikhail Mitrofanov <mk.mitrofamov@outlook.com>
2023-08-26 18:46:12 +0300

    replace initialize AddEditLinkViewModel from
    * initialize Timber

--- app/src/main/java/com/lampotrias/links/LinksApplication.kt
+++ app/src/main/java/com/lampotrias/links/LinksApplication.kt
@@ -3,6 +3,8 @@ package com.lampotrias.links
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
 
 @HiltAndroidApp
 class LinksApplication: Application() {
@@ -10,5 +12,7 @@ class LinksApplication: Application() {
 		super.onCreate()
 
 		Fresco.initialize(this)
+
+		Timber.plant(Timber.DebugTree())
 	}
 }
