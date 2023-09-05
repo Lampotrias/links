a10e730723e5635b3a81f3b33490cce12a1b3c87
Mikhail Mitrofanov <mk.mitrofamov@outlook.com>
2023-09-06 00:40:21 +0300

    fix create link from work

--- app/src/main/java/com/lampotrias/links/data/workmanager/MetadataCreateWorker.kt
+++ app/src/main/java/com/lampotrias/links/data/workmanager/MetadataCreateWorker.kt
@@ -6,11 +6,13 @@ import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 import com.lampotrias.links.domain.cases.AddLinkUseCase
+import com.lampotrias.links.domain.cases.GetFoldersUseCase
 import com.lampotrias.links.domain.cases.GetLinkMetadataUseCase
-import com.lampotrias.links.domain.model.asSaveModel
+import com.lampotrias.links.domain.model.LinkSaveModel
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.firstOrNull
 import timber.log.Timber
 
 @HiltWorker
@@ -18,6 +20,7 @@ class MetadataCreateWorker @AssistedInject constructor(
 	@Assisted private val context: Context,
 	@Assisted params: WorkerParameters,
 	private val addLinkUseCase: AddLinkUseCase,
+	private val getFoldersUseCase: GetFoldersUseCase,
 	private val getLinkMetadataUseCase: GetLinkMetadataUseCase,
 ) : CoroutineWorker(context, params) {
 	init {
@@ -35,10 +38,24 @@ class MetadataCreateWorker @AssistedInject constructor(
 		Timber.w("start doWork")
 		inputData.getString(URL_KEY)?.let { url ->
 			Timber.w("start doWork, url: $url")
-			val linkMetadata = getLinkMetadataUseCase.invoke(url)
-			Timber.w("start doWork, response: ${linkMetadata.isSuccess}")
-			linkMetadata.getOrNull()?.let {
-				addLinkUseCase.invoke(it.asSaveModel())
+
+			getFoldersUseCase.invoke().firstOrNull()?.firstOrNull()?.let { defaultFolder ->
+				val linkMetadata = getLinkMetadataUseCase.invoke(url)
+				Timber.w("start doWork, response: ${linkMetadata.isSuccess}")
+				linkMetadata.getOrNull()?.let { remoteLink ->
+					addLinkUseCase.invoke(
+						LinkSaveModel(
+							id = 0L,
+							title = remoteLink.title,
+							description = remoteLink.description,
+							imageUrl = remoteLink.imageUrl,
+							dateCreate = System.currentTimeMillis(),
+							url = url,
+							isFavorite = false,
+							folderId = defaultFolder.id,
+						)
+					)
+				}
 			}
 		}
 		delay(20_000)
--- app/src/main/java/com/lampotrias/links/domain/model/LinkRemoteModel.kt
+++ app/src/main/java/com/lampotrias/links/domain/model/LinkRemoteModel.kt
@@ -6,16 +6,3 @@ data class LinkRemoteModel(
 	val url: String,
 	val imageUrl: String,
 )
-
-fun LinkRemoteModel.asSaveModel(): LinkSaveModel {
-	return LinkSaveModel(
-		id = 0L,
-		title = title,
-		description = description,
-		imageUrl = imageUrl,
-		dateCreate = System.currentTimeMillis(),
-		url = url,
-		isFavorite = false,
-		folderId = 1, // TODO FIX IT
-	)
-}
